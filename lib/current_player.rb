430731a5eb32f143681964eb432e8898affe034b
Liz Burton <liz.burton147@gmail.com>
2017-11-10 22:31:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   count = 0
-  board.each do |move|
-    if move == "X" || move == "O"
+  board.each do |position|
+    if position == "X" || position == "O"
       count++
     end
   end
